314e7bab110220d066976313a28befb43bf25c84
stephenonchieku1 <onchiekustephen901@gmail.com>
2025-02-26 18:30:19 +0300

    update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import AddClothingItem from '@/components/AddClothingItem.vue';
+import EditClothingItem from '@/components/EditClothingItem.vue';
+import Login from '@/views/Login.vue';
+import Register from '@/views/register.vue';
+
+
+const routes = [
+  { path: '/',name: 'Home' ,component: HomeView },
+  { path: '/add', name: 'AddItem', component: AddClothingItem },
+  { path: '/edit/:id', component: EditClothingItem, props: true },
+  { path: '/login',name: 'Login' ,component: Login },
+  { path: '/register',name: 'Register' ,component: Register },
+
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
